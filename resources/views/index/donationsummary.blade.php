d85bb79f916a61d3214f26e687c97123ec192505
orbachinujbuk <orbachinujbuk@gmail.com>
2020-03-25 00:41:55 +0600

    ASDSD34

--- resources/views/index/donationsummary.blade.php
+++ resources/views/index/donationsummary.blade.php
@@ -42,14 +42,14 @@
                 <!-- counter -->
                 <div class="col-md-4 col-sm-4 bottom-margin text-center counter-section wow fadeInUp sm-margin-bottom-ten" data-wow-duration="300ms">
                     <i class="icon-heart medium-icon"></i>
-                    <span class="timer counter-number" data-to="{{ $totaldonationamount->total }}" data-speed="5000"></span>
+                    <span class="timer counter-number" data-to="{{ $totaldonationamount->total }}" data-speed="3000"></span>
                     <span class="counter-title gray-text">Donation (Tk.)</span>
                 </div>
                 <!-- end counter -->
                 <!-- counter -->
                 <div class="col-md-4 col-sm-4 bottom-margin-small text-center counter-section wow fadeInUp xs-margin-bottom-ten" data-wow-duration="900ms">
                     <i class="icon-anchor medium-icon"></i>
-                    <span class="timer counter-number" data-to="3" data-speed="7000"></span>
+                    <span class="timer counter-number" data-to="3" data-speed="9000"></span>
                     <span class="counter-title gray-text">Partners</span>
                 </div>
                 <!-- end counter -->
