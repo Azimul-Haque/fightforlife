ab1bd51f67b776a280f2d12f5a71f4731bf9770c
orbachinujbuk <orbachinujbuk@gmail.com>
2020-03-24 21:23:58 +0600

    SAD33

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -167,7 +167,7 @@ class IndexController extends Controller
     public function donateCancelled()
     {
         Session::flash('info','Donation is cancelled!');
-        return view('index.index');
+        return redirect()->route('index.index');
     }
 
     public function getDonationSummary()
