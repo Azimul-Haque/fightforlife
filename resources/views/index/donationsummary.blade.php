4dea6de7134d372fc93ac930d9e13fdf53106be1
orbachinujbuk <orbachinujbuk@gmail.com>
2020-03-24 20:11:19 +0600

    ASD3

--- resources/views/index/donationsummary.blade.php
+++ resources/views/index/donationsummary.blade.php
@@ -86,6 +86,7 @@
                             </tbody>
                         </table>
                     </div>
+                    {{ $donors->links() }}
                 </div>
             </div>
         </div>
