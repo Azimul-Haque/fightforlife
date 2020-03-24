17a8a6b813aee3a2439eebc78335300586e392f8
orbachinujbuk <orbachinujbuk@gmail.com>
2020-03-25 04:18:22 +0600

    ASd33

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -27,6 +27,11 @@
     <meta name="msapplication-TileColor" content="#252525">
     <meta name="msapplication-TileImage" content="{{ asset('images/favicons/ms-icon-144x144.png') }}">
     <meta name="theme-color" content="#252525">
+
+    <meta property="og:title" content="Fight For Life | IIT, University of Dhaka">
+    <meta property="og:description" content="Fight For Life is a charitable initiative of IITDU Alumni Association along with IITSec, University of Dhaka. Developed by A. H. M. Azimul Haque">
+    <meta property="og:image" content="{{ asset('images/fb_back.jpg') }}">
+    <meta property="og:url" content="{{ url('/') }}">
     <!-- animation -->
     
     <!-- animation -->
