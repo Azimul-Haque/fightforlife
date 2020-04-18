e8d8b29d54c3795d0bcc5757aa5954e05a41f190
orbachinujbuk <orbachinujbuk@gmail.com>
2020-04-19 01:10:35 +0600

    SAD34234

--- resources/views/index/contact.blade.php
+++ resources/views/index/contact.blade.php
@@ -67,7 +67,7 @@
                         <input type="hidden" name="contact_sum_result_hidden" value="{{ $contact_sum_result_hidden }}">
                         <input type="text" name="contact_sum_result" id="" class="form-control" placeholder="{{ $contact_num1 }} + {{ $contact_num2 }} = ?" required="">
                         
-                        <button id="contact-us-button" type="submit" class="highlight-button-dark btn btn-small button xs-margin-bottom-five"><i class="fa fa-paper-plane"></i> Send</button>
+                        <button type="submit" class="highlight-button-dark btn btn-small button xs-margin-bottom-five"><i class="fa fa-paper-plane"></i> Send</button>
                     </form>
                 </div>
             </div>
